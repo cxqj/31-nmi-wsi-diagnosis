bc530ece9b5e52e6406c977f7d0a597efe6da1cb
cxqj <43702702+cxqj@users.noreply.github.com>
2020-01-08 22:07:46 +0800

    Update gen_patches.sh

--- anno_parser/gen_patches.sh
+++ anno_parser/gen_patches.sh
@@ -6,5 +6,5 @@
 # # generate low grade patches in val, namely in category "1"
 # python gen_patches.py --anno_type "Pos" --dset "test" --slide_id "1_00161_sub0"
 
-# generate normal patches in train, namely in category "3"
+# generate normal patches in train, namely in category "3"   错了吧
 python gen_patches.py --anno_type "Neg" --dset "train" --slide_id "1_00066_sub0"
